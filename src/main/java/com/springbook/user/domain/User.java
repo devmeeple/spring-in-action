11075c17fd06be6b624afae99b15b8170661e691
TAEK <devmeeple@gmail.com>
2023-08-12 13:44:17 +0900

    초난감 DAO 리팩토링 - 관심사 분리

---
+++ src/main/java/com/springbook/user/domain/User.java
@@ -0,0 +1,11 @@
+package com.springbook.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class User {
+    String id;
+    String name;
+    String password;
+}
