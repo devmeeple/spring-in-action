790094a8567e720fce5da00677f7062ff5193dde
장태근 <devmeeple@gmail.com>
2025-02-08 18:04:07 +0900

    [2-4] add spring data jpa to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,9 @@ dependencies {
     // Database
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 kotlin {
