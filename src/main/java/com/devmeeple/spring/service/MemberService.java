05e85339be029117ad040d875f1161e8f1853575
장태근 <devmeeple@gmail.com>
2025-06-27 23:56:56 +0900

    회원 관리 예제(백엔드 개발) - 회원 서비스 개발

---
+++ src/main/java/com/devmeeple/spring/service/MemberService.java
@@ -0,0 +1,33 @@
+package com.devmeeple.spring.service;
+
+import com.devmeeple.spring.domain.Member;
+import com.devmeeple.spring.repository.MemberRepository;
+import com.devmeeple.spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+}
