e8e7174829c757fa5b560f1989eae91a03b7797b
장태근 <devmeeple@gmail.com>
2025-07-25 23:38:17 +0900

    스프링 핵심 원리 이해2(객체 지향 원리 적용) - 스프링으로 전환하기

--- src/main/java/com/devmeeple/spring/basic/OrderApp.java
+++ src/main/java/com/devmeeple/spring/basic/OrderApp.java
@@ -5,12 +5,18 @@ import com.devmeeple.spring.basic.member.Member;
 import com.devmeeple.spring.basic.member.MemberService;
 import com.devmeeple.spring.basic.order.Order;
 import com.devmeeple.spring.basic.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
