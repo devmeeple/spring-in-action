acbe55a39f86101de31ade916550ec060efbc275
TaeGeun Jang <devmeeple@gmail.com>
2025-07-29 23:50:16 +0900

    싱글톤 컨테이너 - 싱글톤 패턴

--- src/test/java/com/devmeeple/spring/basic/singleton/SingletonTest.java
+++ src/test/java/com/devmeeple/spring/basic/singleton/SingletonTest.java
@@ -24,4 +24,19 @@ class SingletonTest {
 //        memberService1 != memberService2
         assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    @Test
+    void singletonServiceTest() {
+//        new SingletonService()
+
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+//        singletonService1 == singletonService2
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
 }
