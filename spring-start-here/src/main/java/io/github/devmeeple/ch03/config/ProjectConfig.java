356bfc1160f33fcd7e5f3cd41bd6bf2bb6a14a7d
TaeGeun Jang <devmeeple@gmail.com>
2025-11-14 06:34:28 +0900

    @Autowired로 클래스 필드를 이용한 값 주입

--- spring-start-here/src/main/java/io/github/devmeeple/ch03/config/ProjectConfig.java
+++ spring-start-here/src/main/java/io/github/devmeeple/ch03/config/ProjectConfig.java
@@ -1,8 +1,5 @@
 package io.github.devmeeple.ch03.config;
 
-import io.github.devmeeple.ch03.beans.Parrot;
-import io.github.devmeeple.ch03.beans.Person;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,18 +7,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ProjectConfig {
 
-    @Bean
-    public Parrot parrot() {
-        Parrot parrot = new Parrot();
-        parrot.setName("Koko");
-        return parrot;
-    }
-
-    @Bean
-    public Person person(Parrot parrot) {
-        Person person = new Person();
-        person.setName("Ella");
-        person.setParrot(parrot);
-        return person;
-    }
 }
