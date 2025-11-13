72414619235d87a1b50e54a78a2befb522839308
TaeGeun Jang <devmeeple@gmail.com>
2025-11-13 10:58:59 +0900

    스프링 교과서(Spring Start Here) 모듈 추가

---
+++ spring-start-here/src/test/java/io/github/devmeeple/SpringStartHereApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devmeeple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartHereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
