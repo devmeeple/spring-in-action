e537ff00d88b43a79a1da7e699c09cdeacabfa98
TaeGeun Jang <devmeeple@gmail.com>
2025-11-13 15:55:14 +0900

    게시글 조회 3 - 게시글 여러개 조회
    - h2 데이터베이스 사용

--- hodolman/src/test/java/io/github/devmeeple/api/contorller/PostControllerTest.java
+++ hodolman/src/test/java/io/github/devmeeple/api/contorller/PostControllerTest.java
@@ -115,4 +115,30 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.content").value("bar"))
                 .andDo(print());
     }
+
+    @DisplayName("글 여러개 조회")
+    @Test
+    void test5() throws Exception {
+        Post post1 = postRepository.save(Post.builder()
+                .title("title_1")
+                .content("content_1")
+                .build());
+        Post post2 = postRepository.save(Post.builder()
+                .title("title_2")
+                .content("content_2")
+                .build());
+
+        mockMvc.perform(get("/posts")
+                        .contentType(APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].id").value(post1.getId()))
+                .andExpect(jsonPath("$[0].title").value("title_1"))
+                .andExpect(jsonPath("$[0].content").value("content_1"))
+                .andExpect(jsonPath("$[1].id").value(post2.getId()))
+                .andExpect(jsonPath("$[1].title").value("title_2"))
+                .andExpect(jsonPath("$[1].content").value("content_2"))
+                .andDo(print());
+    }
 }
