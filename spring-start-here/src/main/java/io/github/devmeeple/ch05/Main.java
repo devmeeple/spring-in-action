73825677ec2fd31508d51f9dfaf77e48f9d678f9
TaeGeun Jang <devmeeple@gmail.com>
2025-11-17 07:21:47 +0900

    지연(Lazy) 인스턴스 초기화

--- spring-start-here/src/main/java/io/github/devmeeple/ch05/Main.java
+++ spring-start-here/src/main/java/io/github/devmeeple/ch05/Main.java
@@ -1,11 +1,16 @@
 package io.github.devmeeple.ch05;
 
 import io.github.devmeeple.ch05.config.ProjectConfig;
+import io.github.devmeeple.ch05.services.CommentService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        System.out.println("Before retrieving the CommentService");
+        CommentService commentService = context.getBean(CommentService.class);
+        System.out.println("After retrieving the CommentService");
     }
 }
