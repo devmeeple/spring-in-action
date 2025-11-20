d50971a0feda5a2124f70a6d4a7aff50233cd64b
TaeGeun Jang <devmeeple@gmail.com>
2025-11-20 11:43:43 +0900

    스프링 MVC로 웹 애플리케이션 구현(정적 콘텐츠)

---
+++ spring-start-here/src/main/java/io/github/devmeeple/ch07/controllers/MainController.java
@@ -0,0 +1,13 @@
+package io.github.devmeeple.ch07.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home.html";
+    }
+}
