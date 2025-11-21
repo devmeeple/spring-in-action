2e21731180dba4f7f07968603c46eaddece092e3
TaeGeun Jang <devmeeple@gmail.com>
2025-11-21 11:15:25 +0900

    뷰로 데이터를 전송하는 방법(Model)

---
+++ spring-start-here/src/main/java/io/github/devmeeple/ch08/controllers/MainController.java
@@ -0,0 +1,16 @@
+package io.github.devmeeple.ch08.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("mainControllerCh08")
+public class MainController {
+
+    @RequestMapping("/ch08/home")
+    public String home(Model page) {
+        page.addAttribute("username", "Katy");
+        page.addAttribute("color", "red");
+        return "home.html";
+    }
+}
