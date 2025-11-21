00c1b652bc8a0bf020f031d400e833fdb74a2660
TaeGeun Jang <devmeeple@gmail.com>
2025-11-21 11:19:46 +0900

    요청 매개변수 사용

--- spring-start-here/src/main/java/io/github/devmeeple/ch08/controllers/MainController.java
+++ spring-start-here/src/main/java/io/github/devmeeple/ch08/controllers/MainController.java
@@ -3,14 +3,15 @@ package io.github.devmeeple.ch08.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller("mainControllerCh08")
 public class MainController {
 
     @RequestMapping("/ch08/home")
-    public String home(Model page) {
+    public String home(@RequestParam(required = false) String color, Model page) {
         page.addAttribute("username", "Katy");
-        page.addAttribute("color", "red");
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
