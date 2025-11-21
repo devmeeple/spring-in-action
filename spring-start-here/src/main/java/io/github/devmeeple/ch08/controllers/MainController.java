9096c97cfba9119508185a4c2e33c6938ec8a1ae
TaeGeun Jang <devmeeple@gmail.com>
2025-11-21 11:31:04 +0900

    경로 매개변수 사용

--- spring-start-here/src/main/java/io/github/devmeeple/ch08/controllers/MainController.java
+++ spring-start-here/src/main/java/io/github/devmeeple/ch08/controllers/MainController.java
@@ -2,18 +2,17 @@ package io.github.devmeeple.ch08.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller("mainControllerCh08")
 public class MainController {
 
-    @RequestMapping("/ch08/home")
+    @RequestMapping("/ch08/home/{color}")
     public String home(
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) String color,
+            @PathVariable String color,
             Model page) {
-        page.addAttribute("username", name);
+        page.addAttribute("username", "Katy");
         page.addAttribute("color", color);
         return "home.html";
     }
