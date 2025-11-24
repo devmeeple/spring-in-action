3ffed2b6bb32e1e3daa529783cefa8e6e7dfc7fe
TaeGeun Jang <devmeeple@gmail.com>
2025-11-24 20:26:29 +0900

    REST 엔드포인트 구현 개선(@RestController 사용)

--- spring-start-here/src/main/java/io/github/devmeeple/ch10/controllers/HelloController.java
+++ spring-start-here/src/main/java/io/github/devmeeple/ch10/controllers/HelloController.java
@@ -1,19 +1,16 @@
 package io.github.devmeeple.ch10.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @ResponseBody
     @GetMapping("/ch10/hello")
     public String hello() {
         return "Hello!";
     }
 
-    @ResponseBody
     @GetMapping("/ch10/ciao")
     public String ciao() {
         return "Ciao!";
