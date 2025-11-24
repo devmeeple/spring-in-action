8831dd51e95534850391cc96485e3883d8d444f8
TaeGeun Jang <devmeeple@gmail.com>
2025-11-24 23:33:59 +0900

    요청 본문(@RequestBody)을 사용하여 클라이언트의 데이터 가져오기

--- spring-start-here/src/main/java/io/github/devmeeple/ch10/controllers/PaymentController.java
+++ spring-start-here/src/main/java/io/github/devmeeple/ch10/controllers/PaymentController.java
@@ -1,24 +1,22 @@
 package io.github.devmeeple.ch10.controllers;
 
 import io.github.devmeeple.ch10.model.PaymentDetails;
-import io.github.devmeeple.ch10.service.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController("paymentControllerCh10")
 public class PaymentController {
 
-    private final PaymentService paymentService;
-
-    public PaymentController(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
 
     @PostMapping("/ch10/payment")
-    public ResponseEntity<PaymentDetails> makePayment() {
-        PaymentDetails paymentDetails = paymentService.processPayment();
+    public ResponseEntity<PaymentDetails> makePayment(@RequestBody PaymentDetails paymentDetails) {
+        logger.info("Received payment " + paymentDetails.getAmount());
 
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
