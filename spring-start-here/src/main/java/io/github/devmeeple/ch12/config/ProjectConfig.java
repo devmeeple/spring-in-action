4cff84743966aa5d70528e0a023621361afc7368
TaeGeun Jang <devmeeple@gmail.com>
2025-11-26 23:58:49 +0900

    사용자 정의 DataSource 빈 사용

---
+++ spring-start-here/src/main/java/io/github/devmeeple/ch12/config/ProjectConfig.java
@@ -0,0 +1,33 @@
+package io.github.devmeeple.ch12.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration("projectConfigCh12")
+public class ProjectConfig {
+
+    @Value("${custom.datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${custom.datasource.username}")
+    private String dataSourceUsername;
+
+    @Value("${custom.datasource.password}")
+    private String dataSourcePassword;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setJdbcUrl(dataSourceUrl);
+        dataSource.setUsername(dataSourceUsername);
+        dataSource.setPassword(dataSourcePassword);
+        dataSource.setConnectionTimeout(1000);
+
+        return dataSource;
+    }
+}
