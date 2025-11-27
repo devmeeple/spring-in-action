742ea0ea45ef3d7ffae11ac09bdf6fd581c16cc7
TaeGeun Jang <devmeeple@gmail.com>
2025-11-27 22:23:03 +0900

    계좌 테이블 쿼리
    - 테이블 생성
    - 데이터 추가

---
+++ spring-start-here/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+-- Ch13. 스프링 앱에서 트랜잭션 사용
+INSERT INTO account VALUES (NULL, 'Helen Down', 1000);
+INSERT INTO account VALUES (NULL, 'Peter Read', 1000);
--- spring-start-here/src/main/resources/schema.sql
+++ spring-start-here/src/main/resources/schema.sql
@@ -1,7 +1,18 @@
-DROP TABLE IF EXISTS purchase CASCADE;
+-- Ch12. 스프링 앱에서의 데이터 소스 사용
 
-CREATE TABLE IF NOT EXISTS purchase (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    product VARCHAR(50) NOT NULL,
-    price DOUBLE NOT NULL
+-- DROP TABLE IF EXISTS purchase CASCADE;
+--
+-- CREATE TABLE IF NOT EXISTS purchase (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     product VARCHAR(50) NOT NULL,
+--     price DOUBLE NOT NULL
+-- );
+
+-- Ch13. 스프링 앱에서 트랜잭션 사용
+DROP TABLE IF EXISTS account CASCADE;
+
+CREATE TABLE account (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    amount DOUBLE NOT NULL
 );
