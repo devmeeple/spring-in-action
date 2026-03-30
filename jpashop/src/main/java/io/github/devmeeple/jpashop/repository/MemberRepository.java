b2ccd1796990ad70b824fcc07ad55b8b79375a50
TaeGeun Jang <devmeeple@gmail.com>
2026-03-31 08:55:04 +0900

    feat: 회원 리포지토리 개발

---
+++ jpashop/src/main/java/io/github/devmeeple/jpashop/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package io.github.devmeeple.jpashop.repository;
+
+import io.github.devmeeple.jpashop.domain.Member;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("SELECT m FROM Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("SELECT m FROM Member m WHERE m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
