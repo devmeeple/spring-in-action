17c270532f665f461a28d3abc888d8d083452cf1
장태근 <devmeeple@gmail.com>
2026-03-30 20:21:06 +0900

    feat: 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 추가

---
+++ jpashop/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.5"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "jpabook"
+version = "0.0.1-SNAPSHOT"
+description = "김영한 '실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발'"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // 웹 애플리케이션
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // 데이터베이스 접근
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // 개발용 데이터베이스
+    implementation("org.springframework.boot:spring-boot-h2console")
+    runtimeOnly("com.h2database:h2")
+
+    // 개발 편의
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // 테스트
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ jpashop/src/test/java/io/github/devmeeple/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devmeeple.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
