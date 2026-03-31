acf06ff6c3714b046742c00086c937d64bcabe15
TaeGeun Jang <devmeeple@gmail.com>
2026-03-31 10:06:52 +0900

    feat: 상품 엔티티 개발(비즈니스 로직 추가)
    - NotEnoughStockException 추가

--- jpashop/src/main/java/io/github/devmeeple/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/io/github/devmeeple/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package io.github.devmeeple.jpashop.domain.item;
 
 import io.github.devmeeple.jpashop.domain.Category;
+import io.github.devmeeple.jpashop.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,4 +27,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
