d2dbfff23bc62d485a47aaef6ceca4f31accf885
장태근 <devmeeple@gmail.com>
2026-03-31 19:23:22 +0900

    feat: 주문 검색 기능 개발(동적 쿼리)
    - JPQL로 처리

--- jpashop/src/main/java/io/github/devmeeple/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/io/github/devmeeple/jpashop/repository/OrderRepository.java
@@ -1,9 +1,17 @@
 package io.github.devmeeple.jpashop.repository;
 
+import io.github.devmeeple.jpashop.domain.Member;
 import io.github.devmeeple.jpashop.domain.Order;
+import io.github.devmeeple.jpashop.domain.OrderSearch;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,5 +27,66 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) { ... }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        // language=JPQL
+        String jpql = "SELECT o FROM Order o JOIN o.member m";
+        boolean isFirstCondition = true;
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " WHERE";
+                isFirstCondition = false;
+            } else {
+                jpql += " AND";
+            }
+            jpql += " o.status = :status";
+        }
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " WHERE";
+                isFirstCondition = false;
+            } else {
+                jpql += " AND";
+            }
+            jpql += " m.name LIKE :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000); // 최대 1000건
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+    }
+
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Order, Member> m = o.join("member", JoinType.INNER); // 회원과 조인
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); // 최대 1000건
+        return query.getResultList();
+    }
 }
