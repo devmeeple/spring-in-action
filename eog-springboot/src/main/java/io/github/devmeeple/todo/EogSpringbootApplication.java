f86ba3ce3e35f4fc2f92e32aca3bb5c2a01dcd8a
TaeGeun Jang <devmeeple@gmail.com>
2026-04-02 14:40:45 +0900

    feat: 스프링 부트 개발자 온보딩 가이드 3장 인메모리 기반의 To-Do 리스트 REST API 서버 만들기 추가

---
+++ eog-springboot/src/main/java/io/github/devmeeple/todo/EogSpringbootApplication.java
@@ -0,0 +1,13 @@
+package io.github.devmeeple.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EogSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EogSpringbootApplication.class, args);
+    }
+
+}
---
+++ eog-springboot/src/test/java/io/github/devmeeple/todo/EogSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devmeeple.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EogSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
