2f905687b0e4938660eac6b2d6b4cfde3b07e8b9
TaeGeun Jang <devmeeple@gmail.com>
2026-04-02 16:09:30 +0900

    feat: To-Do 모델 정의

---
+++ eog-springboot/src/main/java/io/github/devmeeple/todo/model/Todo.java
@@ -0,0 +1,19 @@
+package io.github.devmeeple.todo.model;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    private Long id;
+
+    @NotNull
+    private String title;
+    private String description;
+    private boolean completed;
+}
