3eeec67f364b1c3f537bce2f15fe3473a4bad3c3
TaeGeun Jang <devmeeple@gmail.com>
2026-04-06 14:09:29 +0900

    feat(minilog-jpa): 팔로우 요청 및 응답 DTO 추가

---
+++ eog-springboot/src/main/java/io/github/devmeeple/minilog/dto/FollowRequestDto.java
@@ -0,0 +1,14 @@
+package io.github.devmeeple.minilog.dto;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+public class FollowRequestDto {
+
+    @NonNull
+    private Long followerId;
+
+    @NonNull
+    private Long followeeId;
+}
---
+++ eog-springboot/src/main/java/io/github/devmeeple/minilog/dto/FollowResponseDto.java
@@ -0,0 +1,16 @@
+package io.github.devmeeple.minilog.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+@Builder
+public class FollowResponseDto {
+
+    @NonNull
+    private Long followerId;
+
+    @NonNull
+    private Long followeeId;
+}
