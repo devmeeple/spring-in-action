07d745ccc8a14cdb4881fc3a4cbeebd2e231793f
TaeGeun Jang <devmeeple@gmail.com>
2026-04-06 15:45:55 +0900

    feat(minilog-jpa): FollowController 추가
    - 팔로우
    - 언팔로우
    - 팔로잉 목록 조회

---
+++ eog-springboot/src/main/java/io/github/devmeeple/minilog/controller/FollowController.java
@@ -0,0 +1,61 @@
+package io.github.devmeeple.minilog.controller;
+
+import io.github.devmeeple.minilog.dto.FollowRequestDto;
+import io.github.devmeeple.minilog.dto.FollowResponseDto;
+import io.github.devmeeple.minilog.service.FollowService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/follow")
+public class FollowController {
+
+    private final FollowService followService;
+
+    @Autowired
+    public FollowController(FollowService followService) {
+        this.followService = followService;
+    }
+
+    @PostMapping
+    @Operation(summary = "팔로우")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "404", description = "사용자 없음")
+    })
+    public ResponseEntity<FollowResponseDto> follow(@RequestBody FollowRequestDto request) {
+        Long followerId = request.getFollowerId();
+        Long followeeId = request.getFolloweeId();
+
+        FollowResponseDto follow = followService.follow(followerId, followeeId);
+        return ResponseEntity.ok(follow);
+    }
+
+    @DeleteMapping("/{followerId}/{followeeId}")
+    @Operation(summary = "언팔로우")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "404", description = "사용자 없음")
+    })
+    public ResponseEntity<Void> unfollow(@PathVariable Long followerId, @PathVariable Long followeeId) {
+        followService.unfollow(followerId, followeeId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{followerId}")
+    @Operation(summary = "팔로잉 목록 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "404", description = "사용자 없음")
+    })
+    public ResponseEntity<List<FollowResponseDto>> getFollowList(@PathVariable Long followerId) {
+        List<FollowResponseDto> follows = followService.getfollowList(followerId);
+        return ResponseEntity.ok(follows);
+    }
+}
