0f549318dfc60ae48d5fc2f375fca097fb5f3515
TaeGeun Jang <devmeeple@gmail.com>
2026-04-06 14:10:56 +0900

    feat(minilog-jpa): 엔티티와 DTO간의 매퍼 추가

---
+++ eog-springboot/src/main/java/io/github/devmeeple/minilog/util/EntityDtoMapper.java
@@ -0,0 +1,39 @@
+package io.github.devmeeple.minilog.util;
+
+import io.github.devmeeple.minilog.dto.ArticleResponseDto;
+import io.github.devmeeple.minilog.dto.FollowResponseDto;
+import io.github.devmeeple.minilog.dto.UserResponseDto;
+import io.github.devmeeple.minilog.entity.Article;
+import io.github.devmeeple.minilog.entity.Follow;
+import io.github.devmeeple.minilog.entity.User;
+
+public class EntityDtoMapper {
+
+    public static ArticleResponseDto toDto(Article article) {
+        return ArticleResponseDto.builder()
+                .articleId(article.getId())
+                .content(article.getContent())
+                .authorId(article.getAuthor().getId())
+                .authorName(article.getAuthor().getUsername())
+                .createdAt(article.getCreatedAt())
+                .build();
+    }
+
+    public static FollowResponseDto toDto(Follow follow) {
+        return FollowResponseDto.builder()
+                .followerId(follow.getFollower().getId())
+                .followeeId(follow.getFollowee().getId())
+                .build();
+    }
+
+    public static UserResponseDto toDto(User user) {
+        return UserResponseDto.builder().id(user.getId()).username(user.getUsername()).build();
+    }
+
+    public static Follow toEntity(Long followerId, Long followeeId) {
+        return Follow.builder()
+                .follower(User.builder().id(followerId).build())
+                .followee(User.builder().id(followeeId).build())
+                .build();
+    }
+}
