5a5920d40e08f9bebdbe98c89289a12bf21e2707
TaeGeun Jang <devmeeple@gmail.com>
2026-04-06 23:01:02 +0900

    fix(minilog-jpa): JPA Auditing 활성화를 위한 @EnableJpaAuditing 추가

--- eog-springboot/src/main/java/io/github/devmeeple/minilog/EogSpringbootApplication.java
+++ eog-springboot/src/main/java/io/github/devmeeple/minilog/EogSpringbootApplication.java
@@ -2,12 +2,13 @@ package io.github.devmeeple.minilog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EogSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EogSpringbootApplication.class, args);
     }
-
 }
