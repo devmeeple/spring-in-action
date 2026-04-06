7348d070d3cf8dd008211b54eaffe5a91d4d5dc0
TaeGeun Jang <devmeeple@gmail.com>
2026-04-06 13:25:07 +0900

    refactor(minilog-jpa): 프로젝트 초기화 및 기존 클래스 제거

--- eog-springboot/src/main/java/io/github/devmeeple/minilog/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.devmeeple.minilog.repository;
-
-import io.github.devmeeple.minilog.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends JpaRepository<Todo, Long> {}
